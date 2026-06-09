84c0c374f108959afdd9f50496ab9d634f643bf8
StrelnikovPavlo <126115541+StrelnikovPavlo@users.noreply.github.com>
2026-06-09 20:21:21 +0200

    add swagger setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
 	const PORT = process.env.PORT || 5100
 	const app = await NestFactory.create(AppModule)
 
 	app.setGlobalPrefix('api')
+
+	const config = new DocumentBuilder()
+		.setTitle('Trainify API')
+		.setVersion('1.0')
+		.build()
+
+	const documentFactory = () => SwaggerModule.createDocument(app, config)
+
+	SwaggerModule.setup('api/docs', app, documentFactory)
 
 	await app.listen(PORT, () =>
 		console.log(`Server started on port ${process.env.PORT}`)
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,32 +1,44 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { UsersService } from './users.service'
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
 	@Post()
+	@ApiOperation({ summary: 'Create user' })
+	@ApiResponse({ status: 200 })
 	create(@Body() createUserDto: CreateUserDto) {
 		return this.usersService.create(createUserDto)
 	}
 
 	@Get()
+	@ApiOperation({ summary: 'Get all users' })
+	@ApiResponse({ status: 200 })
 	findAll() {
 		return this.usersService.findAll()
 	}
 
 	@Get(':id')
+	@ApiOperation({ summary: 'Get user' })
+	@ApiResponse({ status: 200 })
 	findOne(@Param('id') id: string) {
 		return this.usersService.findOne(+id)
 	}
 
 	@Put(':id')
+	@ApiOperation({ summary: 'Update data user' })
+	@ApiResponse({ status: 200 })
 	update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
 		return this.usersService.update(+id, updateUserDto)
 	}
 
+	@ApiOperation({ summary: 'Remove user' })
+	@ApiResponse({ status: 200 })
 	@Delete(':id')
 	remove(@Param('id') id: string) {
 		return this.usersService.remove(+id)
