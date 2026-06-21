304596e99efb5adc3965bccf919421fd6afc2e4a
StrelnikovPavlo <126115541+StrelnikovPavlo@users.noreply.github.com>
2026-06-21 12:04:50 +0200

    add auth module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 
 @Module({
@@ -9,7 +10,8 @@ import { UsersModule } from './users/users.module'
 			cache: true, // only production
 			isGlobal: true
 		}),
-		UsersModule
+		UsersModule,
+		AuthModule
 	],
 	controllers: []
 })
